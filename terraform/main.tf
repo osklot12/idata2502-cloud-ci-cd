4bb7b4d29cda0403a30dd736cd8f118cbba690e5
Oskar <osklot12@gmail.com>
2024-11-09 00:06:39 +0100

    Implemented network creation and refactored terraform file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,141 +1,72 @@
+# specifying provider, region and project
 provider "google" {
   project = "idata2502-ci-cd"
   region  = "europe-north1"
 }
 
-# google compute instance for frontend
-resource "google_compute_instance" "svelte_frontend" {
-  name         = "svelte-frontend"
-  machine_type = "e2-micro"
-  zone         = "europe-north1-a"
-
-  # configuring boot image
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      size  = 10
-    }
-  }
-
-  # configuring network interface
-  network_interface {
-    network       = "default"
-    access_config {}
-  }
-
-  # using the terraform service account
-  service_account {
-    email = "terraform-service-account@idata2502-ci-cd.iam.gserviceaccount.com"
-    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
-  }
-
-  # setting up public ssh key
-  metadata = {
-    ssh-keys = "debian:${file("${path.module}/../.ssh/github_actions_key.pub")}"
-  }
-
-  # setting up environment
-  metadata_startup_script = <<-EOT
-    #!/bin/bash
-    apt-get update
-    apt-get install -y docker.io
-    systemctl enable docker
-    systemctl start docker
-    usermod -aG docker debian
-  EOT
-
-  # setting frontend tag for network rule purpose
-  tags = ["frontend"]
+# setting variables
+variable "machine_type" {
+  description = "Machine type for the instances"
+  default = "e2-micro"
 }
 
-# google compute instance for spring
-resource "google_compute_instance" "spring_backend" {
-  name         = "spring-backend"
-  machine_type = "e2-micro"
-  zone         = "europe-north1-a"
-
-  # configuring boot image
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      size  = 10
-    }
-  }
-
-  # configuring network interface
-  network_interface {
-    network       = "default"
-    access_config {}
-  }
-
-  # using the terraform service account
-  service_account {
-    email = "terraform-service-account@idata2502-ci-cd.iam.gserviceaccount.com"
-    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
-  }
-
-  # setting up public ssh key
-  metadata = {
-    ssh-keys = "debian:${file("${path.module}/../.ssh/github_actions_key.pub")}"  # Public key for the 'debian' user
-  }
-
-  # setting up environment
-  metadata_startup_script = <<-EOT
-    #!/bin/bash
-    apt-get update
-    apt-get install -y docker.io
-    systemctl enable docker
-    systemctl start docker
-    usermod -aG docker debian
-  EOT
+variable "image" {
+  description = "Image for the instances"
+  default = "debian-cloud/debian-11"
 }
 
-# google compute instance for postgresql db
-resource "google_compute_instance" "postgresql_db" {
-  name         = "postgresql-db"
-  machine_type = "e2-micro"
-  zone         = "europe-north1-a"
+variable "region" {
+  description = "Region for deployment"
+  default = "europe-north1"
+}
 
-  # configuring boot image
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      size  = 10
-    }
-  }
+variable "zone" {
+  description = "Zone for deployment"
+  default = "europe-north1-a"
+}
 
-  # configuring network interface
-  network_interface {
-    network       = "default"
-    access_config {}
-  }
+variable "service_account_email" {
+  description = "Email of service account"
+  default = "terraform-service-account@idata2502-ci-cd.iam.gserviceaccount.com"
+}
 
-  # using the terraform service account
-  service_account {
-    email = "terraform-service-account@idata2502-ci-cd.iam.gserviceaccount.com"
-    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
-  }
+variable "service_account_scope" {
+  description = "Scope of the service account"
+  default = "https://www.googleapis.com/auth/cloud-platform"
+}
 
-  # setting up public ssh key
-  metadata = {
-    ssh-keys = "debian:${file("${path.module}/../.ssh/github_actions_key.pub")}"  # Public key for the 'debian' user
-  }
+variable "public_ssh_key_path" {
+  description = "Path of the public SSH key for instances"
+  default = "../.ssh/github_actions_key.pub"
+}
 
-  # setting up environment
-  metadata_startup_script = <<-EOT
-    #!/bin/bash
-    apt-get update
-    apt-get install -y docker.io
-    systemctl enable docker
-    systemctl start docker
-    usermod -aG docker debian
-  EOT
+variable "subnet_cidr" {
+  description = "CIDR range for the subnet"
+  default = "10.0.0.0/24"
+}
+
+# keeping the retrieval of ssh key content centralized
+locals {
+  public_ssh_key_content = file("${path.module}/${var.public_ssh_key_path}")
+}
+
+# creating a dedicated network for the application
+resource "google_compute_network" "tomorrow_network" {
+  name = "tomorrow-app-network"
+}
+
+# setting up a subnet
+resource "google_compute_subnetwork" "tomorrow_subnetwork" {
+  name = "tomorrow-app-subnetwork"
+  ip_cidr_range = var.subnet_cidr
+  region = var.region
+  network = google_compute_network.tomorrow_network.id
 }
 
 # setting firewall rule for http traffic to the frontend
 resource "google_compute_firewall" "allow_http_frontend" {
   name = "allow-http-frontend"
-  network = "default"
+  network = google_compute_network.tomorrow_network.id
 
   # allowing tcp traffic on port 80
   allow {
@@ -148,4 +79,184 @@ resource "google_compute_firewall" "allow_http_frontend" {
 
   # setting target tags
   target_tags = ["frontend"]
+}
+
+# google compute instance for frontend
+resource "google_compute_instance" "svelte_frontend" {
+  name         = "svelte-frontend"
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  # configuring boot image
+  boot_disk {
+    initialize_params {
+      image = var.image
+      size  = 10
+    }
+  }
+
+  # configuring network interface
+  network_interface {
+    network = google_compute_network.tomorrow_network.id
+    subnetwork = google_compute_subnetwork.tomorrow_subnetwork.id
+    access_config {}
+  }
+
+  # using the terraform service account
+  service_account {
+    email = var.service_account_email
+    scopes = [var.service_account_scope]
+  }
+
+  # setting up public ssh key
+  metadata = {
+    ssh-keys = "debian:${local.public_ssh_key_content}"
+  }
+
+  # setting up environment
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    set -e
+    apt-get update || { echo "Failed to update packages"; exit 1; }
+    apt-get install -y docker.io || { echo "Failed to install Docker"; exit 1; }
+    systemctl enable docker || { echo "Failed to enable Docker"; exit 1; }
+    systemctl start docker || { echo "Failed to start Docker"; exit 1; }
+    usermod -aG docker debian || { echo "Failed to add user to Docker group"; exit 1; }
+    echo "Startup script completed successfully" >> /var/log/startup-script.log
+  EOT
+
+  # setting frontend tag for network rule purpose
+  tags = ["frontend"]
+
+  # adding labels
+  labels = {
+    environment = "development"
+    app = "tomorrow"
+  }
+
+  # declaring dependencies
+  depends_on = [google_compute_network.tomorrow_network, google_compute_subnetwork.tomorrow_subnetwork]
+}
+
+# google compute instance for spring
+resource "google_compute_instance" "spring_backend" {
+  name         = "spring-backend"
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  # configuring boot image
+  boot_disk {
+    initialize_params {
+      image = var.image
+      size  = 10
+    }
+  }
+
+  # configuring network interface
+  network_interface {
+    network = google_compute_network.tomorrow_network.id
+    subnetwork = google_compute_subnetwork.tomorrow_subnetwork.id
+    access_config {}
+  }
+
+  # using the terraform service account
+  service_account {
+    email = var.service_account_email
+    scopes = [var.service_account_scope]
+  }
+
+  # setting up public ssh key
+  metadata = {
+    ssh-keys = "debian:${local.public_ssh_key_content}"
+  }
+
+  # setting up environment
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    set -e
+    apt-get update || { echo "Failed to update packages"; exit 1; }
+    apt-get install -y docker.io || { echo "Failed to install Docker"; exit 1; }
+    systemctl enable docker || { echo "Failed to enable Docker"; exit 1; }
+    systemctl start docker || { echo "Failed to start Docker"; exit 1; }
+    usermod -aG docker debian || { echo "Failed to add user to Docker group"; exit 1; }
+    echo "Startup script completed successfully" >> /var/log/startup-script.log
+  EOT
+
+  # adding labels
+  labels = {
+    environment = "development"
+    app = "tomorrow"
+  }
+
+  # declaring dependencies
+  depends_on = [google_compute_network.tomorrow_network, google_compute_subnetwork.tomorrow_subnetwork]
+}
+
+# google compute instance for postgresql db
+resource "google_compute_instance" "postgresql_db" {
+  name         = "postgresql-db"
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  # configuring boot image
+  boot_disk {
+    initialize_params {
+      image = var.image
+      size  = 10
+    }
+  }
+
+  # configuring network interface
+  network_interface {
+    network = google_compute_network.tomorrow_network.id
+    subnetwork = google_compute_subnetwork.tomorrow_subnetwork.id
+    access_config {}
+  }
+
+  # using the terraform service account
+  service_account {
+    email = var.service_account_email
+    scopes = [var.service_account_scope]
+  }
+
+  # setting up public ssh key
+  metadata = {
+    ssh-keys = "debian:${local.public_ssh_key_content}"  # Public key for the 'debian' user
+  }
+
+  # setting up environment
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    set -e
+    apt-get update || { echo "Failed to update packages"; exit 1; }
+    apt-get install -y docker.io || { echo "Failed to install Docker"; exit 1; }
+    systemctl enable docker || { echo "Failed to enable Docker"; exit 1; }
+    systemctl start docker || { echo "Failed to start Docker"; exit 1; }
+    usermod -aG docker debian || { echo "Failed to add user to Docker group"; exit 1; }
+    echo "Startup script completed successfully" >> /var/log/startup-script.log
+  EOT
+
+  # adding labels
+  labels = {
+    environment = "development"
+    app = "tomorrow"
+  }
+
+  # declaring dependencies
+  depends_on = [google_compute_network.tomorrow_network, google_compute_subnetwork.tomorrow_subnetwork]
+}
+
+# outputting the frontend instance ip
+output "frontend_instance_ip" {
+  value = google_compute_instance.svelte_frontend.network_interface[0].access_config[0].nat_ip
+}
+
+# outputting the spring backend instance ip
+output "backend_instance_ip" {
+  value = google_compute_instance.spring_backend.network_interface[0].access_config[0].nat_ip
+}
+
+# outputting the postgresql db instance ip
+output "database_instance_ip" {
+  value = google_compute_instance.postgresql_db.network_interface[0].access_config[0].nat_ip
 }
