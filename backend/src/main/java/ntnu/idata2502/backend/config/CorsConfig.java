f26ddc40c46db9d7fbb278e849bd4d32355ceabf
Oskar <osklot12@gmail.com>
2024-11-14 12:05:53 +0100

    Fixed cors config file

--- backend/src/main/java/ntnu/idata2502/backend/config/CorsConfig.java
+++ backend/src/main/java/ntnu/idata2502/backend/config/CorsConfig.java
@@ -8,30 +8,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-   @Bean
-   public WebMvcConfigurer corsConfigurer() {
-       return new WebMvcConfigurer() {
-           @Override
-           public void addCorsMappings(CorsRegistry reg) {
-               String frontendUrl = System.getenv("FRONTEND_URL");
-               reg.addMapping("/**")
-<<<<<<< HEAD:backend/src/main/java/ntnu/idata2502/backend/CorsConfig.java
-<<<<<<< HEAD:backend/src/main/java/ntnu/idata2502/backend/CorsConfig.java
-                       .allowedOrigins("http://localhost")
-=======
-                       .allowedOrigins(
-                               frontendUrl != null ? frontendUrl : "http://localhost:5173"
-                       )
->>>>>>> 700247c (Created api for backend and added some styling):backend/src/main/java/ntnu/idata2502/backend/config/CorsConfig.java
-=======
-                       .allowedOrigins(
-                               frontendUrl != null ? frontendUrl : "http://localhost:5173"
-                       )
->>>>>>> backend:backend/src/main/java/ntnu/idata2502/backend/config/CorsConfig.java
-                       .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                       .allowedHeaders("*")
-                       .allowCredentials(true);
-           }
-       };
-   }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry reg) {
+                String frontendUrl = System.getenv("FRONTEND_URL");
+                reg.addMapping("/**")
+                        .allowedOrigins(
+                                frontendUrl != null ? frontendUrl : "http://localhost:5173"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
