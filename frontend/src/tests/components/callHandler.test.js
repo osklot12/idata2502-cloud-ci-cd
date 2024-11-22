492d10e5a5b8e12a1bbe448b61e474056ff1c1ba
Oskar <osklot12@gmail.com>
2024-11-22 21:27:37 +0100

    Added tests for login and logout using the callhandler

--- frontend/src/tests/components/callHandler.test.js
+++ frontend/src/tests/components/callHandler.test.js
@@ -1,25 +1,52 @@
-import {describe, it, expect} from "vitest";
-import {token, userId} from "../../stores/authStore.js";
+import {describe, it, expect, beforeEach} from "vitest";
+import {token, userId, username} from "../../stores/authStore.js";
 import {get} from "svelte/store";
 import {CallHandler} from "../../services/callHandler.js";
 import {TestApi} from "../../services/api/testApi.js";
 
+let testApi;
+let callHandler;
+
 describe("Tests logging in", () => {
+    beforeEach(() => {
+        testApi = new TestApi();
+        callHandler = new CallHandler(testApi);
+    });
+
     it("should set the token", async () => {
-        const testApi = new TestApi();
-        const callHandler = new CallHandler(testApi);
-
         await callHandler.login(testApi.mockUsername, testApi.mockPassword);
-
         expect(get(token)).toBe(testApi.mockToken);
     });
 
     it("should set the userid", async () => {
-        const testApi = new TestApi();
-        const callHandler = new CallHandler(testApi);
-
         await callHandler.login(testApi.mockUsername, testApi.mockPassword);
-
         expect(get(userId)).toBe(testApi.mockId);
     });
+
+    it("should set the username", async () => {
+        await callHandler.login(testApi.mockUsername, testApi.mockPassword);
+        expect(get(username)).toBe(testApi.mockUsername);
+    })
+});
+
+describe("Tests logging out", () => {
+    beforeEach(() => {
+        testApi = new TestApi();
+        callHandler = new CallHandler(testApi);
+    });
+
+    it("should clear token", async () => {
+        await callHandler.logout();
+        expect(get(token)).toBe(null);
+    });
+
+    it("should clear userId", async () => {
+        await callHandler.logout();
+        expect(get(userId)).toBe(null);
+    })
+
+    it("should clear username", async () => {
+        await callHandler.logout();
+        expect(get(username)).toBe(null);
+    })
 });
