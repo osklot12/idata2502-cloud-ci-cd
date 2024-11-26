5ba3adfd54705c9d15896b51ad5ce197aaa1dead
EpicDome <chris8br@gmail.com>
2024-11-26 13:12:01 +0100

    Added syntax error to Task.svelte

--- frontend/src/components/task/Task.svelte
+++ frontend/src/components/task/Task.svelte
@@ -7,7 +7,7 @@
     import User from "../../classes/User.js";
     import AssigneeHandler from "./AssigneeHandler.svelte";
 
-    export let task;
+    export let task;afshgfkasgfka;;;;;;:
 
     let isMinimized = true;
     let isEditing = false;
